5f55f9ad27a3b768282b33b28381f80fbc8188fb
luoyi <46178521+luoyi58624@users.noreply.github.com>
2025-02-02 22:22:45 +0800

    新增 HomePage 页面 (#2)
    * chore: 新增 home 页面
    
    * chore: 新增容器

---
+++ lib/pages/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('首页'),
+      ),
+      body: Container(
+        width: 100,
+        height: 100,
+        color: Colors.green,
+      ),
+    );
+  }
+}
